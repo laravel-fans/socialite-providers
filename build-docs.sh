05d3242c639ca6dfec86c590850fb586444bc2b6
sink <sinkcup@gmail.com>
2019-05-28 10:49:03 +0800

    new: CI deploy docs to gh-pages

---
+++ build-docs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p docs
+cp README.md docs/
+echo "site_name: 'Laravel Socialite Providers'
+theme: 'readthedocs'
+nav:
+    - index: 'README.md'" > mkdocs.yml
+
+for file in `ls */composer.json`; do
+    package_id=`node getValueFromJsonFile.js $file`
+    echo $package_id
+    dir_name=`dirname $file`
+    cp $dir_name/README.md docs/$package_id.md
+    grep $package_id.md mkdocs.yml || echo "    - $dir_name: '$package_id.md'" >> mkdocs.yml
+done
+
+mkdocs build --clean
