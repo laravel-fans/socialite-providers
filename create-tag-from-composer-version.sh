7741a1ad44eb9e6b7826e86937560e435af4c706
sinkcup <sinkcup@gmail.com>
2019-07-17 11:56:35 +0800

    opt: create tag from version of composer.json

---
+++ create-tag-from-composer-version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+file='composer.json'
+echo -e "\ncreate tag"
+version=`node getValueFromJsonFile.js $file version`
+tag_exists=`git ls-remote origin refs/tags/$version`
+if [ -z $tag_exists ]; then
+    git tag -a $version -m 'new version'
+    git push --tags
+fi
